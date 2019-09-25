5f2cff266a750438bdd4308b066b6984b72877e1
Mario <themario80@gmail.com>
2019-09-24 23:52:31 -0500

    Importing Project to Github. This is a test project and I want to see how well it gets integrated into Github

---
+++ RegexTutorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+            string test = "This is [:test 5] test.";
+            string output;
+
+            output = Regex.Replace(test, @"[.] *", "");
+
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
